c1ae14f8673ef41d7a2c80a795da48330261bcef
katlford31 <katlford31@hotmail.co.uk>
2018-01-24 12:22:54 +0000

    Change to View and Hand
    Move split card into player hand, dont need hand class, Changes to playgameview

--- src/commandline/playgameview.java
+++ src/commandline/playgameview.java
@@ -26,30 +26,32 @@ public void Menuview() {
 }
 
 public void GameView(Card[] player) {
-	System.out.print("Select number of players between 2 and 5");
-	Scanner players = new Scanner(System.in);
-	while(players.hasNext());
-	String numplayer = players.next();
-	int NumPlayer = Integer.parseInt(numplayer);
-	if((NumPlayer <= 2) && (NumPlayer >= 4)) {
-	System.out.print("You've selected " + player);
-	} else {
-	System.out.print("Invalid number of players");
-	}
+//	System.out.print("Select number of players between 2 and 5");
+//	Scanner players = new Scanner(System.in);
+//	while(players.hasNext());
+//	String numplayer = players.next();
+//	int NumPlayer = Integer.parseInt(numplayer);
+//	if((NumPlayer <= 2) && (NumPlayer >= 4)) {
+//	System.out.print("You've selected " + player);
+//	} else {
+//	System.out.print("Invalid number of players");
+//	}
 	int i = 1;
 	while((player.length >= 1) && (player.length <= 39)){
-	//Call select category method 
+	//Call select category method
 	//Call method for selecting current card 
-	int currentcard = 1; //Just to stop errors need to pass the value of current card from the method for selecting card 
+	int currentcard = 1; //Just to stop errors need to pass the value of current card from the method for selecting card, need index of current card 
 	System.out.print(player[currentcard]); 
 	//Call method for comparing values 
 	System.out.print("You currently have " + player.length + "Cards");
 	i++; 
 	if(player.length <= 0) {
 	System.out.print("You've lost the game");
+	Menuview();
 	}
 	if(player.length >= 40) {
 	System.out.print("You've won the game"); 
+	Menuview();
 	}
 	}
 }
