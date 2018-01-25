1ffc3be13382b5aec4bab8b4925fb6b47263cd9e
katlford31 <katlford31@hotmail.co.uk>
2018-01-25 13:30:42 +0000

    Player class

--- src/commandline/Player.java
+++ src/commandline/Player.java
@@ -1,18 +1,53 @@
-// Robbie
+package commandline;
+
 public class Player {
-//	IVS
-	private PlayerHand hand;
-	private String name;
-	private static int playerNum;
-	
-	public Player() {
-		playerNum++;
-		name = "Player " + playerNum;
-		hand = new PlayerHand();
-	}
-	
-	public AIPlayer()
-	{
-		
-	}
+
+private PlayerHand hand;
+
+private String AID1;
+private String AID2;
+private String AID3;
+private String AID4;
+private String PlayerID;
+private PlayerHand AI1hand;
+private PlayerHand AI2hand;
+private PlayerHand AI3hand;
+private PlayerHand AI4hand;
+private String playername;
+ 
+public Player(String Playername) 
+{
+	//Creates human player ID
+	PlayerID = "Player" + playername;
+	//Creates players hand 
+	hand = new PlayerHand();
+}
+public void AIPlayer()
+{
+	//Creates first AI ID
+	AID1 = "AI1"; 
+	//first AI hand
+	AI1hand = new PlayerHand();
+}
+public void AIPlayer2() 
+{
+	//Creates second AI ID
+	AID2 = "AI2";
+	//Second AI hand 
+	AI2hand = new PlayerHand();
+}
+public void AIPlayer3()
+{ 
+	//Creates third AI ID
+	AID3 = "AI3";
+	//Third AI hand 
+	AI3hand = new PlayerHand(); 
+}
+public void AIPlayer4() 
+{
+	//Creates fourth AI ID
+	AID4 = "AI4";
+	//Fourth AI hand 
+	AI4hand = new PlayerHand(); 
+}
 }
